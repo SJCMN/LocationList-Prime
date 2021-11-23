6cc2d5970faccea0fd4fa4ff1e782763aa6ef566
SJCMN <sam.clavette@gmail.com>
2021-11-22 22:11:36 -0600

    add mui to list

--- src/components/ListOutput/ListOutput.jsx
+++ src/components/ListOutput/ListOutput.jsx
@@ -1,8 +1,10 @@
 import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -34,58 +36,43 @@ function ListOutput ({item}) {
     }
 
     return (
+        <ListItem key={item.id} 
+          secondaryAction={
+            <IconButton edge="end" aria-label="delete">
+              <HighlightOffIcon >
+              onClick={() =>  (dispatch({ type: "DELETE_ITEM", payload: item.id }))}
+              </HighlightOffIcon>
+            </IconButton>
+          }
+        disablePadding
+        >
+         
+          <ListItemButton 
+            onClick={() =>  handleUpdate(item)} 
+            dense>
+            <ListItemIcon>
+              <Checkbox
+              edge="start"
+              checked={item.hidden}
+              tabIndex={-1}
+              disableRipple
+              inputProps={{ 'aria-labelledby': item }}
+              />
+             
+              </ListItemIcon>
+              <ListItemText 
+                className={item.hidden === false ? "standard" : "hidden"}
+                primary=
+                {(mode === 'SHOP') ? 
+                <>{item.department_id} {item.aisle_id} {item.keyword_search}</> 
+                : 
+                <>{item.keyword_search}</> }
+              />
+          </ListItemButton>
 
-
-        <li key={item.id} >
-            <div className="listItem">
-                <button onClick={() =>  handleUpdate(item)}>archive</button>
-
-                    <p className={item.hidden === false ? "standard" : "hidden"}>
-
-                    {(mode === 'SHOP') ? 
-                    <>{item.department_id} {item.aisle_id} {item.keyword_search}</> 
-                    : 
-                    <>{item.keyword_search}</> }
-                    
-                    </p>
-
-                <button onClick={() =>  (dispatch({ type: "DELETE_ITEM", payload: item.id }))}>delete</button>
-            </div>
-        </li>
+        </ListItem>
 
     );
 };
 
 export default ListOutput;
-
-
-// <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-// {[0, 1, 2, 3].map((value) => {
-//   const labelId = `checkbox-list-label-${value}`;
-
-//   return (
-//     <ListItem
-//       key={value}
-//       secondaryAction={
-//         <IconButton edge="end" aria-label="comments">
-//           <CommentIcon />
-//         </IconButton>
-//       }
-//       disablePadding
-//     >
-//       <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-//         <ListItemIcon>
-//           <Checkbox
-//             edge="start"
-//             checked={checked.indexOf(value) !== -1}
-//             tabIndex={-1}
-//             disableRipple
-//             inputProps={{ 'aria-labelledby': labelId }}
-//           />
-//         </ListItemIcon>
-//         <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-//       </ListItemButton>
-//     </ListItem>
-//   );
-// })}
-// </List> 
